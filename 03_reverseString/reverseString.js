0b05998fa555412d8cc7d9197a7556fc49a8114d
haychpea <106247334+haychpea@users.noreply.github.com>
2023-04-22 10:27:54 +0100

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    let revArray = array.reverse()
+    return revArray.join('')
 };
 
 // Do not edit below this line
