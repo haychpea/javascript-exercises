c800eddf34917a170af68d7160b7dab345166aa3
haychpea <106247334+haychpea@users.noreply.github.com>
2023-04-22 10:04:22 +0100

    Complete Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+// First Successful Attempt   
+    /*if (num === 0 || string === ''){
+        return ''
+    } else if (num === 1) {
+        return string
+    } else if (num < 0){
+        return 'ERROR'
+    } else {
+       // for (let i = 0; i <= num; i++){
+        return Array(num+1).join(string)
+    }
+};*/
 
-};
-
+// Second Optimised attempt
+    let result = '';
+    if (num < 0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++){
+        result += string;
+    }
+    return result
+}
 // Do not edit below this line
 module.exports = repeatString;
