eec93166d84d8dad2b64b15bd3757d355e5094a6
haychpea <106247334+haychpea@users.noreply.github.com>
2023-04-24 00:13:02 +0100

    Complete Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round(((temp-32)*(5/9))*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(((temp*(9/5)+32))*10)/10
 };
+
+// Having done a bit of work on a unit converter, this one was relatively simple
 
 // Do not edit below this line
 module.exports = {
