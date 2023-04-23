434cba6d35ad177e6d35a58b7360361aed861c89
haychpea <106247334+haychpea@users.noreply.github.com>
2023-04-23 23:28:09 +0100

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    if (!Number.isInteger(val1) || !Number.isInteger(val2)){
+        return 'ERROR';
+    } else if (val1 < 0 || val2 < 0){
+        return 'ERROR'
+    } else {
+        if (val1 > val2){
+            const rev = val1;
+            val1 = val2;
+            val2 = rev
+        }
+        let sum = 0;
+        for (let i = val1; i < val2 + 1; i++){
+            sum += i;
+        }
+        return sum;
+    } 
+}        
 
-};
+/*
+ Had to peek at answer for this one, could not figure out how the values were switched round
+ Also, my initial for loop criteria was incorrect and was keeping sum equal to 0
+*/
 
 // Do not edit below this line
 module.exports = sumAll;
