979939b2703d5ffae4406293716a0d9847b3b426
haychpea <106247334+haychpea@users.noreply.github.com>
2023-04-23 22:26:51 +0100

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    // First test using a rest parameter
+    for (const arg of args){
+        for (const val of arr){
+            let index = 0
+            if (val === arg){
+                index = arr.indexOf(arg);
+                if (index > -1){
+                    arr.splice(index, 1);
+                }
+            }        
+        }
+    }
+    return arr
 };
 
-// Do not edit below this line
+
+/* References
+https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+*/
+//Do not edit below this line
 module.exports = removeFromArray;
